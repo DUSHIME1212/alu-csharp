4f95fbc25042c4af011db0084c6d1e5d3e7a41b4
DUSHIME1212 <mdonavan33@gmail.com>
2025-05-14 06:59:54 +0200

    Add compiled DLLs for 5-print_float project in Debug configuration for .NET 7.0

--- csharp-hello_world/8-print_variables/obj/Debug/net7.0/8-print_variables.AssemblyInfo.cs
+++ csharp-hello_world/8-print_variables/obj/Debug/net7.0/8-print_variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8-print_variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0015ba8e86550c8dbb259036da6c908343a8d8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("8-print_variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8-print_variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
