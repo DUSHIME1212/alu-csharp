62febb726512ff18d7e0a3846aa45edada2d4abb
DUSHIME1212 <mdonavan33@gmail.com>
2025-05-15 06:49:11 +0200

    Add initial project files and configuration for 3-print_alphabt
    - Created DLL and reference files for the 3-print_alphabt project targeting .NET 7.0.
    - Added project.assets.json to manage project dependencies and configurations.
    - Included project.nuget.cache for NuGet package management.
    - Set up assembly attributes and metadata for the application.

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'A'; letter <= 'Z'; letter++)
+        {
+            Console.Write(letter);
+        }
+    }
+}
