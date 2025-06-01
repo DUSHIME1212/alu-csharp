b12b022df4b05ec5704d842077a941b434e1c066
DUSHIME1212 <mdonavan33@gmail.com>
2025-06-01 23:32:02 +0200

    Add initial implementation of linked list deletion functionality
    - Created the necessary DLL and reference files for the linked list delete project.
    - Added project.assets.json to manage project dependencies and configurations for .NET 7.0.
    - Included project.nuget.cache to track NuGet package cache information.

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0 || n >= myLList.Count) 
+            return 0; 
+
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; i < n; i++)
+        {
+            current = current.Next; 
+        }
+
+        return current.Value; 
+    }
+}
