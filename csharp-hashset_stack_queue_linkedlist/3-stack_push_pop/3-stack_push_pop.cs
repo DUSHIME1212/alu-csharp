414fab01affc8cc9e7aaa4b1d0d9070b512e66d9
DUSHIME1212 <mdonavan33@gmail.com>
2025-06-05 19:07:10 +0200

    Refactor MyStack class to improve readability and fix conditional logic in Info method

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,50 +1,52 @@
 using System;
 using System.Collections.Generic;
 
-class MyStack
+public static class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         
-        Console.WriteLine($"Number of items: {aStack.Count}");
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
         
-        
-        if (aStack.Count > 0)
-        {
-            Console.WriteLine($"Top item: {aStack.Peek()}");
-        }
-        else
+        if (aStack.Count == 0)
         {
             Console.WriteLine("Stack is empty");
         }
-        
+        else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+
         
         bool containsSearch = aStack.Contains(search);
-        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
-        
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+
         
         if (containsSearch)
         {
-            Stack<string> tempStack = new Stack<string>();
-            while (aStack.Count > 0)
-            {
-                string item = aStack.Pop();
-                if (item == search)
-                {
-                    break;
-                }
-                tempStack.Push(item);
-            }
             
+            Stack<string> tempStack = new Stack<string>();
+
+            
+            while (aStack.Peek() != search)
+            {
+                tempStack.Push(aStack.Pop());
+            }
+
+            
+            aStack.Pop();
+
             
             while (tempStack.Count > 0)
             {
                 aStack.Push(tempStack.Pop());
             }
         }
-        
+
         
         aStack.Push(newItem);
+
         
         return aStack;
     }
