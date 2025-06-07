7c09e9a1e836926db1e0b9042e45315c50a08111
DUSHIME1212 <mdonavan33@gmail.com>
2025-06-07 11:08:39 +0200

    Refactor GetNode method to handle null LinkedList and improve readability

--- csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -1,24 +1,25 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 
-public class LList
+class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
-        if (myLList == null || n < 0 || n >= myLList.Count)
-            return 0;
-
-        LinkedListNode<int> current = myLList.First;
-        if (current == null)
-            return 0;
-
-        for (int i = 0; i < n; i++)
+        if (n < 0 || n >= myLList.Count)
         {
-            current = current.Next;
-            if (current == null)
-                return 0;
+            return 0;
         }
-
-        return current.Value;
+        
+        int index = 0;
+        foreach (int value in myLList)
+        {
+            if (index == n)
+            {
+                return value;
+            }
+            index++;
+        }
+        
+        return 0;
     }
 }
